8ac62882bc99f356aaea556eaf98c8cc8a264832
Myst33d <notmyst33d@gmail.com>
2023-11-23 00:01:40 +0600

    Use deprecated API anyway

--- app/src/main/java/com/bmdelacruz/vgp/ControllerActivity.kt
+++ app/src/main/java/com/bmdelacruz/vgp/ControllerActivity.kt
@@ -105,30 +105,25 @@ class ControllerActivity : AppCompatActivity() {
                 createThumbStickPositionResetListener(ThumbStickType.Right)
         }
 
-        @Suppress("DEPRECATION")
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
-            window.decorView.setOnSystemUiVisibilityChangeListener {
-                vm.isSystemUiVisible.value = it and View.SYSTEM_UI_FLAG_FULLSCREEN == 0
-            }
+        window.decorView.setOnSystemUiVisibilityChangeListener {
+            vm.isSystemUiVisible.value = it and View.SYSTEM_UI_FLAG_FULLSCREEN == 0
+        }
 
-            lifecycleScope.launchWhenCreated {
-                systemUiVisibilityControlChannel.consumeEach { isVisible ->
-                    window.decorView.systemUiVisibility = if (!isVisible) {
-                        View.SYSTEM_UI_FLAG_IMMERSIVE or
-                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                                View.SYSTEM_UI_FLAG_FULLSCREEN
-                    } else {
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    }
+        lifecycleScope.launchWhenCreated {
+            systemUiVisibilityControlChannel.consumeEach { isVisible ->
+                window.decorView.systemUiVisibility = if (!isVisible) {
+                    View.SYSTEM_UI_FLAG_IMMERSIVE or
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                            View.SYSTEM_UI_FLAG_FULLSCREEN
+                } else {
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 }
             }
-        } else {
-            TODO()
         }
 
         soundPool = SoundPool.Builder()
@@ -200,11 +195,9 @@ class ControllerActivity : AppCompatActivity() {
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
 
-        if (hasFocus && Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
-            lifecycleScope.launchWhenResumed {
-                delay(300)
-                systemUiVisibilityControlChannel.send(false)
-            }
+        lifecycleScope.launchWhenResumed {
+            delay(300)
+            systemUiVisibilityControlChannel.send(false)
         }
     }
 
